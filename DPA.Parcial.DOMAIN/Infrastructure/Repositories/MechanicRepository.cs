cf60e4a3f2cb9681ddbe09671a26d9ae6f7c7632
Javier Yuen <yuenjavier7@gmail.com>
2024-10-12 20:19:24 -0500

    Part1

---
+++ DPA.Parcial.DOMAIN/Infrastructure/Repositories/MechanicRepository.cs
@@ -0,0 +1,64 @@
+﻿using DPA.Parcial.DOMAIN.Core.Entities;
+using DPA.Parcial.DOMAIN.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA.Parcial.DOMAIN.Infrastructure.Repositories
+{
+    public class MechanicRepository
+    {
+        private readonly Parcial22200555Context _dbContext;
+
+        public MechanicRepository(Parcial22200555Context dbContext)
+        {
+            _dbContext=dbContext;
+        }
+
+        //Sincrona
+        public IEnumerable<Mechanic> GetMechanicSync()
+        {
+            var mechanic = _dbContext.Mechanic.ToList();
+            return mechanic;
+        }
+
+        //Asincrona
+        public async Task<IEnumerable<Mechanic>> GetMechanies()
+        {
+            var mechanic = await _dbContext.Mechanic.ToListAsync();
+            return mechanic;
+        }
+
+        public async Task<Mechanic> GetMechanicById(int id)
+        {
+            var mechanic = await _dbContext.Mechanic.Where(c => c.Id == id).FirstOrDefaultAsync();
+            return mechanic;
+        }
+
+        public async Task<int> Insert(Mechanic mechanic)
+        {
+            await _dbContext.Mechanic.AddAsync(mechanic);
+            int rows = await _dbContext.SaveChangesAsync();
+            return rows > 0 ? mechanic.Id : -1;
+        }
+
+        public async Task<bool> Update(Mechanic mechanic)
+        {
+            _dbContext.Mechanic.Update(mechanic);
+            int rows = await _dbContext.SaveChangesAsync();
+            return rows > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+
+            var category = await GetMechanicById(id);
+            if (category == null) return false;
+            int rows = await _dbContext.SaveChangesAsync();
+            return rows > 0;
+        }
+    }
+}
