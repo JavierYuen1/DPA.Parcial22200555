fce91696ace40c1865c450af6e6dd6d04dbd6140
Javier Yuen <yuenjavier7@gmail.com>
2024-10-12 20:03:17 -0500

    Agregar archivos de proyecto.

---
+++ DPA.Parcial.DOMAIN/Infrastructure/Data/Parcial22200555Context.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using DPA.Parcial.DOMAIN.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DPA.Parcial.DOMAIN.Infrastructure.Data;
+
+public partial class Parcial22200555Context : DbContext
+{
+    public Parcial22200555Context()
+    {
+    }
+
+    public Parcial22200555Context(DbContextOptions<Parcial22200555Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Mechanic> Mechanic { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAP-SU-TIC-01;Database=Parcial22200555;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Mechanic>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.HireDate).HasColumnType("datetime");
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.LastName)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
